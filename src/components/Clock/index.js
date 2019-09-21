e1ac1d64512da7c371504ecae3e104f977e86b32
Kate Howard <k.l.howard.1992@gmail.com>
2019-09-21 19:29:45 +0100

    Make Clock more interesting

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Face, Hand } from './styles';
+import { Face, Hand, LongLine, ShortLine, Middle } from './styles';
 
 function Clock(props) {
 	return (
 		<Face>
+			<LongLine />
+			<LongLine across />
+			{[30, 60, 120, 150].map((angle, index) => (
+				<ShortLine key={index} angle={angle} />
+			))}
 			<Hand {...props} />
+			<Middle />
 		</Face>
 	);
 }
--- src/components/Clock/styles.js
+++ src/components/Clock/styles.js
@@ -4,18 +4,45 @@ export const Face = styled.div`
 	width: 100px;
 	height: 100px;
 	border-radius: 50%;
-	background-color: #eee;
+	background-color: #f7f6c8;
 	position: relative;
 	align-self: center;
 `;
 
 export const Hand = styled.div`
-	width: 2px;
+	width: 4px;
 	height: 45px;
-	background-color: navy;
+	background-color: blue;
 	position: absolute;
-	top: 5px;
-	left: 49px;
-	transform-origin: 0 100%;
-	transform: rotate(${props => props.degrees}deg);
+	transform-origin: 50% 100%;
+	transform: translate(48px, 5px) rotate(${props => props.degrees}deg);
+`;
+
+export const LongLine = styled.div`
+	width: ${props => (props.across ? 100 : 2)}px;
+	height: ${props => (props.across ? 2 : 100)}px;
+	background-color: lightgrey;
+	position: absolute;
+	top: ${props => (props.across ? 49 : 0)}px;
+	left: ${props => (props.across ? 0 : 49)}px;
+`;
+
+export const ShortLine = styled.div`
+	width: 1px;
+	height: 100px;
+	background-image: linear-gradient(lightgrey 15%, #f7f6c8 15% 85%, lightgrey 85%);
+	position: absolute;
+	top: 0;
+	left: 49.5px;
+	transform: rotate(${props => props.angle}deg);
+`;
+
+export const Middle = styled.div`
+	width: 10px;
+	height: 10px;
+	border-radius: 50%;
+	position: absolute;
+	top: 45px;
+	left: 45px;
+	background-color: blue;
 `;
